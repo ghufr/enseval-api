383f27fd793b6da3616406f94c488fbda95387fd
Ghufron Fikrianto <ghufronfr@gmail.com>
2021-06-11 03:06:49 +0700

    Fix migration class

--- database/migrations/2021_06_04_000000_create_customer_table.php
+++ database/migrations/2021_06_04_000000_create_customer_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMaintenanceTable extends Migration
+class CreateCustomerTable extends Migration
 {
     /**
      * Run the migrations.
