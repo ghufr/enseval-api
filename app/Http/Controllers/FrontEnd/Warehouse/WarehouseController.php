de268e0dcdcb2965c24b43ecb32eae9c3be8e01d
ghufr <ghufronfr@gmail.com>
2021-06-12 06:52:22 +0700

    fix typo

--- app/Http/Controllers/FrontEnd/Warehouse/WarehouseController.php
+++ app/Http/Controllers/FrontEnd/Warehouse/WarehouseController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Controllers\Controller;
 
-class StockController extends Controller
+class WarehouseController extends Controller
 {
     /**
      * Display a listing of the resource.
